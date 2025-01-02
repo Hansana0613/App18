063c312ef1c90798887ba6aaa8ad85e352543be0
Hansana Madhuranga <153816119+Hansana0613@users.noreply.github.com>
2025-01-02 13:35:52 +0530

    new note ui added

--- app/src/main/java/lk/javainstitute/app18/MainActivity.java
+++ app/src/main/java/lk/javainstitute/app18/MainActivity.java
@@ -1,6 +1,9 @@
 package lk.javainstitute.app18;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +22,15 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        Button button1 = findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, CreateNoteActivity.class);
+                startActivity(i);
+            }
         });
     }
 }

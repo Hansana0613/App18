03d40311d9c4b1c5fe8cc7b9d0183c5e80a413e4
Hansana Madhuranga <153816119+Hansana0613@users.noreply.github.com>
2025-01-02 13:50:37 +0530

    ui updated

--- app/src/main/java/lk/javainstitute/app18/CreateNoteActivity.java
+++ app/src/main/java/lk/javainstitute/app18/CreateNoteActivity.java
@@ -1,6 +1,10 @@
 package lk.javainstitute.app18;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +23,27 @@ public class CreateNoteActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        Button button2 = findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                EditText editText1 = findViewById(R.id.editTextText1);
+                EditText editText2 = findViewById(R.id.editTextText2);
+
+                if (editText1.getText().toString().isEmpty()) {
+                    Toast.makeText(CreateNoteActivity.this, "Please Fill The Title", Toast.LENGTH_LONG).show();
+
+                } else if (editText2.getText().toString().isEmpty()) {
+                    Toast.makeText(CreateNoteActivity.this, "Please Fill The Content", Toast.LENGTH_LONG).show();
+
+                } else {
+                    //save
+
+                }
+            }
         });
     }
 }
